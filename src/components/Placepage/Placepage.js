6dcbe3108ab7acba44917e1d0a388c0d5f9e51e7
oliviapangkiey <oliviapangkiey07@gmail.com>
2019-09-15 13:19:28 -0700

    place page style1

--- src/components/Placepage/Placepage.js
+++ src/components/Placepage/Placepage.js
@@ -3,10 +3,25 @@ import React, { Component } from "react"
 export default class Placepage extends Component {
   render() {
     return (
-      <div>
-        {this.props.place.address}
+      <div style={{
+        padding: "2rem",
+        // border: "5px solid blue",
+        margin: "1rem",
+        fontWeight: "bold",
+        fontFamily:"montserrat"
+      }}>
+        <h3>{this.props.place.name}</h3>
+        <div style={{
+        // padding: "2rem",
+        // border: "5px solid blue",
+        // margin: "1rem",
+        fontWeight: "normal",
+        fontFamily:"montserrat"
+      }}>
+        Address: {this.props.place.address}
         <br/>
         Starts on monday at: {this.props.place.startsmon_at}
+        </div>
       </div>
     )
   }
