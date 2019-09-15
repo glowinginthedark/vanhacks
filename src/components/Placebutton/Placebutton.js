bb7fbbda07da9505c2eabf7a4a99ac3ee28a947a
oliviapangkiey <oliviapangkiey07@gmail.com>
2019-09-15 13:03:27 -0700

    place button style

--- src/components/Placebutton/Placebutton.js
+++ src/components/Placebutton/Placebutton.js
@@ -5,12 +5,22 @@ export default class Placebutton extends Component {
     return (
       <div style={{
         padding: "2rem",
-        border: "5px solid blue",
+        // border: "5px solid blue",
         margin: "1rem",
+        fontWeight: "bold",
+        fontFamily:"montserrat"
       }} onClick={() => {
         this.props.onClick(this.props.place)
       }}>
-        {this.props.place.address}
+        {this.props.place.name}
+        <div style={{
+          fontWeight: "normal"
+        }}>
+        <br></br>
+        Call: {this.props.place.phone}
+
+        <hr></hr>
+        </div>
       </div>
     )
   }
